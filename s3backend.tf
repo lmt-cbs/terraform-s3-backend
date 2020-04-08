c79e076d9952abb90ce6dd0bf9e37110cc1d0814
lmtbelmontem <lmtbelmonte@gmail.com>
2020-04-08 18:37:41 +0200

    modificacin del fichero s3backend para incluir la llamada al modulo

--- s3backend.tf
+++ s3backend.tf
@@ -1,3 +1,12 @@
 provider "aws" {
   region  = "us-west-2"
 }
+
+module "s3backend" {
+  source = "lmtbelmonte/terraform-s3-backend"
+  namespace     = "pokemon"
+}
+ 
+output "s3backend_config" {
+  value = module.s3backend.config
+}
